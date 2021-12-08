db6212a0c70088bc5629dec1fee068b336d6ae38
sarge846 <sargente4@yahoo.com>
2021-12-07 20:54:59 -0600

    trying to implement API functionality

--- BlackJack/BlackJack/Models/Deck.cs
+++ BlackJack/BlackJack/Models/Deck.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
 
 namespace BlackJack.Models
 {
-    class Deck
+    public class Deck
     {
+        private bool success { get; set; }
+        private string deck_id { get; set; }
+        private int remaining { get; set; }
+        private bool shuffled { get; set; }
+        public List<Card> Hand { get; set; }
+    }
+
+    public class Card
+    {
+        private string image { get; set; }
+        private string value { get; set; }
+        private string suit { get; set; }
+        private string code { get; set; }
     }
 }
---
+++ BlackJack/BlackJack/Models/DeckAPI.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack.Models
+{
+    class DeckAPI
+    {
+        private const string baseUrl = "https://deckofcardsapi.com/api/deck";
+
+        public async static Task<Deck> NewDeck()
+        {
+            Uri request = new Uri($"{baseUrl}/new/shuffle/");
+            Deck deck;
+
+            using (var httpClient = new HttpClient())
+            {
+                var json = await httpClient.GetStringAsync(request);
+                deck = JsonConvert.DeserializeObject<Deck>(json);
+            }
+
+            return deck;
+        }
+
+        public async static Task<Deck> Deal(string deck_id)
+        {
+            Uri request = new Uri($"{baseUrl}/{deck_id}/draw/?count=2");
+
+            Card[] cards;
+
+            for(int i = 0; i < 2; i++)
+            {
+
+            }
+
+
+        }
+    }
+}
