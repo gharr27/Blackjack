db6328d9b7ea18f72108d33a2cb01d99ec8cf6cb
sarge846 <sargente4@yahoo.com>
2021-12-10 19:05:46 -0600

    UI *probably* complete

---
+++ BlackJack/BlackJack/SignInPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace BlackJack
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class SignInPage : Page
+    {
+        public SignInPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void cancelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.GoBack();
+        }
+
+        private void signInBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //add and save new User to DB
+            this.Frame.Navigate(typeof(GamePage));
+        }
+
+        private void signUpBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //search and find User in DB
+            this.Frame.Navigate(typeof(GamePage));
+        }
+    }
+}
